ae84b7f4d3ed68032006231784554997e57b8fec
arannatao <aranna@163.com>
2016-09-05 16:07:59 +0800

    Update LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -4,6 +4,7 @@ public class LongestSubstring {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		String s = in.next();
+		in.close();
 		LongestSubstringSolution solution = new LongestSubstringSolution();
 		int length = solution.lengthOfLongestSubstring(s);
 		System.out.print(length);
